6d469e11ff66c94bb942ae54065107ee9932c8dd
Seonghyeon <97867254+usingnameing@users.noreply.github.com>
2024-08-05 16:05:53 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1096 ms, Memory: 60464 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -3,24 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader=
-                new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer=
-                new StringTokenizer(bufferedReader.readLine());
-        int suNo=Integer.parseInt(stringTokenizer.nextToken());
-        int quizNo=Integer.parseInt(stringTokenizer.nextToken());
-        long[] S=new long[suNo+1];
-        stringTokenizer=new StringTokenizer(bufferedReader.readLine());
-        for(int i=1;i<=suNo;i++){
-            S[i]=S[i-1]+Integer.parseInt(stringTokenizer.nextToken());
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(bf.readLine());
+        int n,q;
+        n=Integer.parseInt(st.nextToken());
+        q=Integer.parseInt(st.nextToken());
+        int[] sum_array=new int[n];
+        int tmp;
+        st=new StringTokenizer(bf.readLine());
+        for(int i=0;i<n;i++){
+            tmp=Integer.parseInt(st.nextToken());
+            if(i==0){
+                sum_array[0]=tmp;
+            }else{
+                sum_array[i]=sum_array[i-1]+tmp;
+            }
         }
-        for(int j=0;j<quizNo;j++){
-            stringTokenizer=new StringTokenizer(bufferedReader.readLine());
-            int i=Integer.parseInt(stringTokenizer.nextToken());
-            int q=Integer.parseInt(stringTokenizer.nextToken());
-            System.out.println(S[q]-S[i-1]);
+        int i,j;
+        for(int k=0;k<q;k++){
+            st=new StringTokenizer(bf.readLine());
+            i= Integer.parseInt(st.nextToken())-1;
+            j=Integer.parseInt(st.nextToken())-1;
+
+            if(i==0){
+                System.out.println(sum_array[j]);
+            }else{
+                System.out.println(sum_array[j]-sum_array[i-1]);
+            }
         }
     }
 }
