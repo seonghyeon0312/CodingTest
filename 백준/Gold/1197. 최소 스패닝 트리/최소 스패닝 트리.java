58572465648cc986c2402a9a5e1f2d9f1b3f86b7
Seonghyeon <97867254+usingnameing@users.noreply.github.com>
2024-09-03 16:21:19 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 484 ms, Memory: 49612 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,76 +1,96 @@
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Optional;
 import java.util.StringTokenizer;
 import java.util.PriorityQueue;
 
 public class Main {
-    static class Edge implements Comparable<Edge> {
-        int s;
-        int e;
-        int w;
-        public Edge(int s,int e,int w){ this.s=s; this.e=e; this.w=w; }
-        public int compareTo(Edge e){
-            if(this.w>e.w){
-                return 1;
-            }else{
-                return -1;
-            }
-        }
-    }
 
-    static int n;
-    static int m;
+    static PriorityQueue<Edge> pQueue=new PriorityQueue<Edge>();
+    static int[] node;
+    static int n,m;
 
-    static PriorityQueue<Edge> edges=new PriorityQueue<>();
-    static int[] distance;
-    static boolean minus=false;
+    static long result=0;
 
     public static void main(String[] args) throws IOException{
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(bf.readLine());
+
         n=Integer.parseInt(st.nextToken());
         m=Integer.parseInt(st.nextToken());
-        distance= new int[n+1];
+
+        node=new int[n+1];
         for(int i=1;i<=n;i++){
-            distance[i]=i;
+            node[i]=i;
         }
+
         for(int i=0;i<m;i++){
             st=new StringTokenizer(bf.readLine());
-            Edge edge=new Edge(Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
-            edges.add(edge);
+            int s=Integer.parseInt(st.nextToken());
+            int e=Integer.parseInt(st.nextToken());
+            int value=Integer.parseInt(st.nextToken());
+            Edge edge = new Edge(s,e,value);
+            pQueue.add(edge);
         }
-        int total_weight=0;
-        int cnt=0;
-        while(cnt!=n-1 && !edges.isEmpty()){
-            Edge now=edges.poll();
-            int start=now.s;
-            int end=now.e;
-            int weight=now.w;
-            if(find(start)!=find(end)){
-                union(start,end);
-                total_weight+=weight;
-                cnt++;
-            }else{
-                continue;
+
+        for(int i=0;i<m;i++){
+            Edge poll = pQueue.poll();
+            boolean same = find(poll.s, poll.e);
+
+            if(same){
+                union(poll.s,poll.e);
+                result+=poll.value;
             }
         }
-        System.out.print(total_weight);
+        System.out.println(result);
     }
-    public static void union(int start, int end){
-        if((distance[start]==start)&&(distance[end]==end)){
-            distance[end]=distance[start];
+    private static void union(int s,int e){
+        if(node[e]!=e){
+            union(s,node[e]);
         }else{
-            int result1=find(start);
-            int result2=find(end);
-            distance[result2]=distance[result1];
+            node[e]=s;
         }
     }
-    public static int find(int n){
-        if(distance[n]!=n){
-            distance[n]=find(distance[n]);
+
+    private static boolean find(int s, int e){
+        if(node[s]!=s){
+            unionFind(s);
         }
-        return distance[n];
+        if(node[e]!=e){
+            unionFind(e);
+        }
+
+        if(node[s]!=node[e]){
+            return true;
+        }else return false;
+    }
+
+    private static int unionFind(int s){
+        if(node[s]==s){
+            return s;
+        }else{
+            int result =unionFind(node[s]);
+            node[s]=result;
+            return result;
+        }
+    }
+    private static class Edge implements Comparable<Edge>{
+        private int s;
+        private int e;
+        private long value;
+
+        public Edge(int s, int e, long value) {
+            this.s = s;
+            this.e = e;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            if(this.value>o.value){
+                return 1;
+            }else return -1;
+        }
     }
 }
