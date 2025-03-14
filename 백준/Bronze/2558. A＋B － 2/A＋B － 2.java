2e052cdce809bf1c3f969fd68c3e5ae200fe6cf5
Seonghyeon <97867254+seonghyeon0312@users.noreply.github.com>
2025-03-14 10:25:20 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -8,9 +8,13 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException{
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt()+sc.nextInt());
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
+        int sum =Integer.parseInt(st.nextToken());
+        st=new StringTokenizer(bf.readLine());
+        sum+=Integer.parseInt(st.nextToken());
+        System.out.println(sum);
     }
 
 }
