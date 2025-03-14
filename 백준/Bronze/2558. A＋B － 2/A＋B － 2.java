ff9c21ce4c82e6df8b174d2f5b88567b939b8861
Seonghyeon <97867254+seonghyeon0312@users.noreply.github.com>
2025-03-14 10:21:02 +0900

    [Bronze V] Title: A+B - 2, Time: 172 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()+sc.nextInt());
+
+    }
+
+}
