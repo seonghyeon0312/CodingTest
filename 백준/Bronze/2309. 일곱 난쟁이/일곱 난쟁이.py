57d33613b4d54aa5a4b28c376df95274ab4fbb56
Seonghyeon <97867254+seonghyeon0312@users.noreply.github.com>
2025-03-16 21:40:30 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,27 +1,21 @@
+
 height = list()
-height2 = list()
+
 sum = 0
 for _ in range(9):
     temp= int(input())
     sum+=temp
     height.append(temp)
-    height2.append(temp)
 
 height.sort()
-height2.sort()
+height_set = set(height)
 target = sum-100
-check = False
-for i in range(9):
-    for j in range(9):
-        if i==j:
-            continue
-        if height[i]+height2[j] == target:
-            idx1 = max(i, j)
-            idx2 = min(i,j)
-            del height[idx1], height[idx2]
-            check =True
-            break
-    if check:
+
+for h in height:
+    target_h = target - h
+    if target_h in height_set and target_h !=h:
+        height.remove(target_h)
+        height.remove(h)
         break
 
 for tall in height:
