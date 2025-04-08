285414dbbefa21f6dca836730187a298b14e5231
Seonghyeon <97867254+seonghyeon0312@users.noreply.github.com>
2025-04-08 13:58:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 212 ms, Memory: 18256 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,36 +1,32 @@
 import java.util.Scanner;
 
-public class Main{
-    static int n,k;
-    static int[][] board;
+public class Main {
+    static int n, k;
     static int[] weight;
-    static int[] values;
-    public static void main(String[] args){
+    static int[] value;
+    static int[] dp;
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
         k = sc.nextInt();
 
-        board= new int[k+1][n+1];
-        weight = new int[n+1];
-        values=new int[n+1];
+        weight = new int[n + 1];
+        value = new int[n + 1];
+        dp = new int[k + 1]; // 무게가 최대 k인 경우까지
 
-        for(int i=1;i<=n;i++){
-            weight[i]=sc.nextInt();
-            values[i]=sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            weight[i] = sc.nextInt();
+            value[i] = sc.nextInt();
         }
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=k;j++) {
-                if(j-weight[i]>=0){
-                    board[j][i]=Math.max(board[j][i-1],values[i]+board[j-weight[i]][i-1]);
-                }else{
-                    board[j][i]=board[j][i-1];
-                }
+        for (int i = 1; i <= n; i++) {
+            for (int j = k; j >= weight[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
             }
         }
 
-
-        System.out.println(board[k][n]);
+        System.out.println(dp[k]);
     }
 }
