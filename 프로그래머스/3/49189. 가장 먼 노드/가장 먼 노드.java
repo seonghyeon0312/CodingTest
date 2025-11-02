aa2094ef7292ed4e6d24d7403a1b25e83aeb63e7
Seonghyeon <97867254+seonghyeon0312@users.noreply.github.com>
2025-11-02 20:28:00 +0900

    [level 3] Title: 가장 먼 노드, Time: 43.23 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,54 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Solution {
+    static List<Integer> distance = new ArrayList<>();
+    static List<List<Integer>> graph = new ArrayList<>();
+    
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        
+        for(int i=0;i<=n;i++){
+            distance.add(50001);
+            graph.add(new ArrayList<>());
+        }
+        
+        distance.set(1, 0);
+        
+        for(int[] vertex : edge){
+            graph.get(vertex[0]).add(vertex[1]);
+            graph.get(vertex[1]).add(vertex[0]);
+        }
+    
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        boolean[] visited = new boolean[n+1];
+        visited[1] = true;
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            
+            for(Integer next : graph.get(node)){
+                if(visited[next]) continue;
+                distance.set(next, distance.get(node)+1);
+                visited[next] = true;
+                queue.add(next);
+            }
+        }
+        
+        int maxDistance = -1;
+        for(Integer dist : distance){
+            if(dist == 50001) continue;
+            
+            maxDistance = Math.max(maxDistance, dist);
+        }
+        
+        for(Integer dist : distance){
+            if(dist == maxDistance) answer++;
+        }
+        return answer;
+    }
+    
+    
+}
