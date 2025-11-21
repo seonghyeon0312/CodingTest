1eb9ca692111be1b4431c1ac523ef2c7ca3aa6cc
Seonghyeon <97867254+seonghyeon0312@users.noreply.github.com>
2025-11-21 17:46:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.71 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 56.8 MB, 시간: 9.78 ms
+메모리: 54.4 MB, 시간: 10.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 12일 00:47:30
+2025년 11월 21일 17:46:10
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -8,35 +8,44 @@ class Solution {
         answer = bfs(maps);
         return answer;
     }
+    
     public int bfs(int[][] maps){
+        Queue<Cord> queue = new LinkedList<>();
         int n = maps.length;
         int m = maps[0].length;
-        int[][] board = new int[n][m];
+        maps[n-1][m-1] = -1;
+        boolean[][] visited = new boolean[n][m];
+        queue.add(new Cord(0,0));
+        visited[0][0] = true;
         int[] dx = {1,0,-1,0};
         int[] dy = {0,1,0,-1};
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> queue = new LinkedList<>();
         
-        int[] start = {0,0};
-        queue.add(start);
-        board[0][0] = 1;
-        board[n-1][m-1]= -1;
-        visited[0][0] = true;
         while(!queue.isEmpty()){
-            int[] cord = queue.poll();
-            
+            Cord now = queue.poll();
+            if(now.x == n-1 && now.y == m-1) break;
             for(int i=0;i<4;i++){
-                int nx = cord[0]+dx[i];
-                int ny = cord[1]+dy[i];
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
                 
-                if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && maps[nx][ny] ==1){
-                    board[nx][ny] = board[cord[0]][cord[1]]+1;
-                    visited[nx][ny] = true;
-                    queue.add(new int[]{nx,ny});
+                if(nx>=0 && nx<n && ny>=0 && ny<m){
+                    if(!visited[nx][ny] && maps[nx][ny] != 0){
+                        queue.add(new Cord(nx,ny));
+                        visited[nx][ny] = true;
+                        maps[nx][ny] = maps[now.x][now.y]+1;
+                    }
                 }
             }
         }
+        return maps[n-1][m-1];
+    }
+    
+    class Cord{
+        int x;
+        int y;
         
-        return board[n-1][m-1];
+        public Cord(int x, int y){
+            this.x=x;
+            this.y=y;
+        }
     }
 }
