0c58dc60469dd6465d2473c1ab0cf547dbe81012
Seonghyeon <97867254+seonghyeon0312@users.noreply.github.com>
2025-11-21 17:03:13 +0900

    [Gold V] Title: 두 용액, Time: 356 ms, Memory: 30952 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,7 +13,7 @@ public class Main {
         for(int i=0;i<n;i++){
             liquidPh.add(Integer.parseInt(st.nextToken()));
         }
-        liquidPh.sort(Comparator.comparingInt(a -> a));
+        Collections.sort(liquidPh);
 
         int left = 0;
         int right = n-1;
