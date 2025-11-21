1af098a7a8852a6c5805596b9bce4a2da99e10f1
Seonghyeon <97867254+seonghyeon0312@users.noreply.github.com>
2025-11-21 17:01:44 +0900

    [Gold V] Title: 두 용액, Time: 376 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+        List<Integer> liquidPh = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for(int i=0;i<n;i++){
+            liquidPh.add(Integer.parseInt(st.nextToken()));
+        }
+        liquidPh.sort(Comparator.comparingInt(a -> a));
+
+        int left = 0;
+        int right = n-1;
+        int minValue = Integer.MAX_VALUE;
+        int[] result = new int[2];
+
+        while(left<right){
+            int value = liquidPh.get(left) + liquidPh.get(right);
+            int absValue = Math.abs(value);
+
+            if(absValue < minValue ){
+                minValue = absValue;
+                result[0] = liquidPh.get(left);
+                result[1] = liquidPh.get(right);
+            }
+
+            if( value < 0){
+                left++;
+            }else if(value > 0){
+                right--;
+            }else{
+                break;
+            }
+        }
+
+        System.out.print(result[0] + " " + result[1]);
+    }
+}
