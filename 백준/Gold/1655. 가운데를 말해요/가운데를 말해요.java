34f6a74914df8238cbceae6fa55eac8ac671a13c
Seonghyeon <97867254+seonghyeon0312@users.noreply.github.com>
2026-02-26 10:42:41 +0900

    [Gold II] Title: 가운데를 말해요, Time: 400 ms, Memory: 33268 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -4,34 +4,33 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
-	static int n;
-	static PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-	static PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    static PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    static PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
     public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	n = Integer.parseInt(st.nextToken());
-    	
-    	for(int i=0;i<n;i++) {
-    		maxHeap.add(Integer.parseInt(br.readLine()));
-    		int minSize = minHeap.size();
-    		int maxSize = maxHeap.size();
-    		
-    		if(maxSize >= minSize + 2) {
-    			int mid = maxHeap.poll();
-    			minHeap.add(mid);
-    		}
-    		
-    		if(!minHeap.isEmpty()){
-    			if(minHeap.peek() < maxHeap.peek()) {
-        			int minTemp = minHeap.poll();
-        			int maxTemp = maxHeap.poll();
-        			maxHeap.add(minTemp);
-        			minHeap.add(maxTemp);
-        		}
-    		}
-    		System.out.println(maxHeap.peek());
-    	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine().trim());
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine().trim());
+
+            // 올바른 힙에 삽입
+            if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+                maxHeap.add(num);
+            } else {
+                minHeap.add(num);
+            }
+
+            // 크기 균형
+            if (maxHeap.size() >= minHeap.size() + 2) {
+                minHeap.add(maxHeap.poll());
+            } else if (maxHeap.size() < minHeap.size()) {
+                maxHeap.add(minHeap.poll());
+            }
+
+            sb.append(maxHeap.peek()).append('\n');
+        }
+        System.out.print(sb);
     }
 }
